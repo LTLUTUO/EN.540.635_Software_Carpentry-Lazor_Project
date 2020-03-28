5f316be5183648d379d4d34cd1b9b0d581876a24
Oops <62154614+xf6d4@users.noreply.github.com>
2020-03-28 19:49:40 -0400

    Update put_block.py
    Continue block class

--- put_block.py
+++ put_block.py
@@ -5,3 +5,14 @@ The code is writen by Tuo Lu and Alex Fan
 
 """
 
+class Block(object):
+    '''
+    This class is for 3 types of blocks: reflect, opaque and refract
+    '''
+    def __init__(self, block_position):
+        '''
+        initial the block's position
+        '''
+        self.block_position = block_position
+        
+    def 
