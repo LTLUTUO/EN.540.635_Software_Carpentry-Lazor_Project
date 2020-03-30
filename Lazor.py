9019500b7e7608d4c4a164c3dda12da31417f492
Oops <62154614+xf6d4@users.noreply.github.com>
2020-03-29 23:47:56 -0400

    Update Lazor.py
    new thoughts

--- Lazor.py
+++ Lazor.py
@@ -23,56 +23,53 @@ class Lazor(object):
         '''
         self.start = start
         self.direction = direction
-        
-    def beam(self):
-        '''
-        this function calculates the slop and intersection for lazor beam
-        '''
-        k = (self.start[1] - self.direction[1]) / (self.start[0] - self.direction[0])
-        b = self.start[1] - k * self.start[0]
-        return k, b
     
-    def lazor_intersect_or_nor(self, query):
+    def return_block_cross(self, grid):
         '''
-        this function calculates if the query point can be intersected
+        this function lazor cross blocks or not
         
         **Parameters**
-            query: *tuple*
-                   the point coordinate want to check.
-                   
-        '''
-        self.query = query
-        k, b = Lazor.beam(self)
-        lazor_vector = (self.direction[0] - self.start[0], self.direction[1] - self.start[1])
-        query_vector = (self.query[0] - self.start[0], self.query[1] - self.start[1])
-        vector_align = lazor_vector[0] * query_vector[1] - query_vector[0] * lazor_vector[1]
-        if self.query[0] * k + b == self.query[1]:
-            if vector_align == 0 and query_vector[0] / lazor_vector[0] > 0:
-                return True
-            else:
-                return False
-        else:
-            return False
+            grid: *tuple*
+                   grid of the game.       
         
-    def between_two_points_or_not(self, candidate, query):
         '''
-        this function checks the query point is falling between lazor start point and candidate point or not
-        
-        **Parameters**
-            candidate: *tuple*
-                       the end point of the line.
-            query: *tuple*
-                   the point coordinate you want to check.
-                   
-        '''
-        self.query = query
-        self.candidate = candidate
-        x_cord = [self.candidate[0], self.start[0]]
-        y_cord = [self.candidate[1], self.start[1]]
-        if self.query[0] >= min(x_cord) and self.query[1] >= min(y_cord) and self.query[0] <= max(x_cord) and self.query[1] <= max(y_cord):
-            return True
-        else:
-            return False
+        self.grid = grid
+
+        # assign lazor at starting position
+        lazor = start
+        block_corss = []
+        while lazor in grid:
+            # calculate the next point lazor will be
+            next_position = [start[0] + direction[0], start[1] + direction [1]]
+            next_block = block_position
+            if next_block not funciton_block:
+                block_cross.append(next_block)
+            elif next_block is function_block:
+                judge what is the block
+                # change derection/slope
+                    k = adsf
+                # contine & change
+                    contine + return new lazor (return_block_cross)
+                # abosor
+                    break, return
             
-                
+            
+        if slope not block:
+            return
+        pass
+    
+    
+    def returnpoint_cross(self):
+        dict 
+        pass
+
+    
+    def goal_search(self, goal):
+        goal_left = []
+        for i in goal:
+            if i not in point_cross:
+                goal_left.append(i)
+        return goal_left
+            
+    goal = goal left
         
