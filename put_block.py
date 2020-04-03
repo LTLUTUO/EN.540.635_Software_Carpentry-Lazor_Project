4e0a50760c3e40823d1111e4aee80a6d53f6851e
Oops <62154614+xf6d4@users.noreply.github.com>
2020-04-03 11:36:39 -0400

    Update put_block.py
    Update put_block function

--- put_block.py
+++ put_block.py
@@ -5,83 +5,40 @@ The code is writen by Tuo Lu and Alex Fan
 
 """
 
-class Block(object):
+def put_block(x_positions, o_positions, next_block, block):
     '''
-    This class is for 3 types of blocks: reflect, opaque and refract
-    '''
-    def __init__(self, block_position):
-        '''
-        initial the block's position
-        '''
-        self.block_position = block_position
-        
-    def intersect_point(self):
-        '''
-        this function returns possible intersection point and the intersection surface on the block.
-        '''
-        x_cord = 2 * self.block_position[0] + 1
-        y_cord = 2 * self.block_position[1] + 1
-        intersect_surface = {(x_cord, y_cord - 1): 'up', (x_cord, y_cord + 1): 'down',
-                             (x_cord + 1, y_cord): 'left', (x_cord - 1, y_cord: 'right')}
-        intersect_point = [(x_cord, y_cord - 1), (x_cord, y_cord + 1), (x_cord - 1, y_cord), 
-                           (x_cord + 1, y_cord)]
-        return intersect_point, intersect_surface
+    this function puts the blocks in the grid
     
-    def reflect(self, lazor_start, lazor_dir):
-        '''
-        this function calculates the reflect block
-        
-        **Parameters**
-            lazor_start: *tuple*
-                         the starting point of lazor
-            lazor_dir: *tuple*
-                        the direction of the lazor
-        
-        '''
-        self.k, self.b = lazor(lazor_start, lazor_dir).line()
-        self.dir = lazor_dir
-        self.start = lazor_start
-        intersect_point, intersect_surface = Block.intersect_point(self)
-        min_distance = 'min'
-        candidate = 'none'
-        out_point = 'none'
-        lazor_in_block = copy.deepcopy(intersect_point)
-        # determine if the lazor is on block
-        if self.start in lazor_in_block:
-            lazor_in_block.remove(self.start)
-            for i in lazor in block:
-                if Lazor(self.start, self.dir).lazor_intersect_or_not(i) is True:
-                    self.k = -1 / self.k
-                    self.b = self.origin_point[1] - self.k * self.dir[0]
-                    candidate = self.startsurface = intersect_surface.get(i)
-                    if surface == 'left' or surface == 'right'
-                        out_point = (self.dir[0], self.k * self.dir[0] + self.b)
-                    else:
-                        out_point = ((self.dir[1] - self.b) / self.k, self.dir[1])
-                else:
-                    pass
-        else:
-            for i in intersect_point:
-                if Lazor(self.start, self.dir).lazor_intersect_or_not(i) is True:
-                    update = np.linalg.norm(np.array(i) - np.array(self.start))
-                    if update < min_distance:
-                        min_distance = update
-                        candidate = i
-                    else:
-                        continue
-                else:
-                    continue
-            if candidate != 'none':
-                self.k = -1 / self.k
-                self.b = candidate[1] - self.k * candidate[0]
-                surface = intersect_surface.get(candidate)
-                if surface == 'left' or urface == 'right':
-                    out_point = (self.start[0], self.k * self.start[0] + self.b)
-                else:
-                    out_point = ((self.start[1] - self.b) / self.k, self.start[1])
-            else:
-                out_point = 'none'
-           
-        return [(self.k, self.b)], candidate, out_point
+    **Parameters**
+        x_positions: *list*
+                      positions for "x" in grid, which mean no block can be put.
+        o_positions: *list*
+                      positions for "o" in grid, which mean any block can be put.
+        next_block: *tuple*
+                    next block position and type.
+        block: *list*
+                a list shows how many each types of blocks given
+    '''    
+    # x_positions = blocks cannot put anything
+    # o_positions = open blocks
+
+    # define next_block to be position + a random block type
+    # block_type = random.choice('ABC')
+    # next_block = [block_position, block_type]    
+    
+    if next_block[0] in x_positions:
+        pass
+    elif next_block[0] in o_positions:
+        # 满足随机选一个block type，同时这个type试题中要求的
+        if next_block[1] == 'A' and block[0] != 0:
+            block = [block[0] - 1, block[1], block[2]]
+        elif next_block[1] == 'B' and block[1] != 0:
+            block = [block[0], block[1] - 1, block[2]]
+        elif next_block[1] == 'C' and block[2] != 0:
+            block = [block[0], block[1], block[2] - 1]
             
-                
+            
+            
+
+            
+            
